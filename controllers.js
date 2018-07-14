9b5dc2ca16282a9aeb424d0a7b466d25645a6bd2
sund0ll <40809399+sund0ll@users.noreply.github.com>
2018-07-14 19:12:36 -0400

    updated

---
+++ controllers.js
@@ -0,0 +1,16 @@
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+
+    $scope.city = cityService.city;
+
+    $scope.$watch('city', function() {
+       cityService.city = $scope.city;
+    });
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+
+    $scope.city = cityService.city;
+
+}]);
